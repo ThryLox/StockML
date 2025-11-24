9166b70caee305e4836b0dc8979619559b681dff
ThryLox <83971708+ThryLox@users.noreply.github.com>
2025-11-24 16:39:01 -0500

    Force model retraining by deleting old models in build script

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r backend/requirements.txt
 
+echo "Removing old models (if any)..."
+rm -rf backend/models
+rm -rf models
+
 echo "Training models..."
 python backend/train_models.py
 
